dc3fdd2beac179dad2cac3da67870a7efbbb88df
Jeff Lee <leejeffc@gmail.com>
2014-02-03 15:43:41 -0800

    Add boilerplate for stack viz

--- scripts/viz-stack.js
+++ scripts/viz-stack.js
@@ -1,23 +1,36 @@
 (function(){
+  var stack = [];
+
+  var push = function(){
+    var input = ''; // TODO; get string from input text field
+    stack.push(input);
+    updateViz();
+    // TODO: animate pushed item landing on the page out of no where
+    // before it gets added to the rest of the circles in the viz
+  };
+
+  var pop = function(){
+    stack.pop();
+    updateViz();
+    // TODO: animate popped item being set aside
+  };
+
+
   var svgWidth = 400;
   var svgHeight = 400;
-
 
   var svgContainer = d3.select('.visualization')
                        .append('svg')
                        .attr("width", svgWidth)
                        .attr("height", svgHeight);
 
-
-  var updateViz = function(data){
+  var updateViz = function(){
     var items = svgContainer.selectAll('circle')
-                            .data(data, String);
+                            .data(stack, String);
 
     items.enter().append('circle');
 
     items.exit().remove();
   };
-
-  updateViz([1,2,3]);
 
 }());
