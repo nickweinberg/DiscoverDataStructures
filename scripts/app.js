e0ce1907da3a3faf99fe4d817391a5aa6b66ea75
Jeff Lee <leejeffc@gmail.com>
2014-02-04 19:43:08 -0800

    Add proper syntax for dependency injection

--- scripts/app.js
+++ scripts/app.js
@@ -1,20 +1,19 @@
-angular.module('DiscoverDataStructsApp', ['ngRoute']).config(function($routeProvider){
-  'use strict';
+angular.module('DiscoverDataStructsApp', ['ngRoute'])
+  .config(['$routeProvider', function($routeProvider){
+    'use strict';
 
-  console.log('hi');
-
-  // Routes
-  // ==========================================================================
-  $routeProvider
-    .when('/', {
-      templateUrl: 'views/home.html',
-      controller: 'HomeCtrl'
-    })
-    .when('/stack', {
-      templateUrl: 'views/stack.html',
-      controller: 'StackCtrl'
-    })
-    .otherwise({
-      redirectTo: '/'
-    });
-});
+    // Routes
+    // ==========================================================================
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/home.html',
+        controller: 'HomeCtrl'
+      })
+      .when('/stack', {
+        templateUrl: 'views/stack.html',
+        controller: 'StackCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
