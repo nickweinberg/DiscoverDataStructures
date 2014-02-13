2e60b251ee74cb60387820c048c5862e37788313
Jeff Lee <leejeffc@gmail.com>
2014-02-13 15:34:39 -0800

    Create app variable to shorten boilerplate

--- scripts/app.js
+++ scripts/app.js
@@ -1,23 +1,23 @@
-angular.module('DiscoverDataStructsApp', ['ngRoute'])
-  .config(['$routeProvider', function($routeProvider){
-    'use strict';
+var app = angular.module('DiscoverDataStructsApp', ['ngRoute']);
+app.config(['$routeProvider', function($routeProvider){
+  'use strict';
 
-    // Routes
-    // ==========================================================================
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/home.html',
-        controller: 'HomeCtrl'
-      })
-      .when('/stack', {
-        templateUrl: 'views/stack.html',
-        controller: 'StackCtrl'
-      })
-      .when('/queue', {
-        templateUrl: 'views/queue.html',
-        controller: 'QueueCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  }]);
+  // Routes
+  // ==========================================================================
+  $routeProvider
+    .when('/', {
+      templateUrl: 'views/home.html',
+      controller: 'HomeCtrl'
+    })
+    .when('/stack', {
+      templateUrl: 'views/stack.html',
+      controller: 'StackCtrl'
+    })
+    .when('/queue', {
+      templateUrl: 'views/queue.html',
+      controller: 'QueueCtrl'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+}]);
